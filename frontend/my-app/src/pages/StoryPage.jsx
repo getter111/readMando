ab17b8757f8bdfc560db2d7842e2286567decbdb
getter111 <ayl@g.clemson.edu>
2025-02-13 14:34:57 -0500

    added api call to generate story on storyPage

--- frontend/my-app/src/pages/StoryPage.jsx
+++ frontend/my-app/src/pages/StoryPage.jsx
@@ -1,8 +1,26 @@
+import {useEffect, useState} from 'react';
+import {axios} from 'axios'
+
 export default function StoryPage() {
+
+    const [story, setStory] = useState("")
+    useEffect(() => {
+        const fetchStory = async () => {
+            try {
+                const response = await axios.get("http://127.0.0.1:8000/generate_story")
+                setStory(response)
+            } catch (error){
+                console.log(error)
+            }
+        };
+        fetchStory()
+    });
+
     return (
-        
         <div>
             <h1>Story Page</h1>
-        </div>
+            {story ? <h3>{story.title}</h3> : <h3>Loading title...</h3>}
+            {story ? <p>{story.content}</p> : <p>Loading story...</p>}
+      </div>
     )
 }
