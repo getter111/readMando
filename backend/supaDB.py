eba0812ae6732a51ceecc0f1a3d6d2ad9461890b
Alex <ayl@g.clemson.edu>
2025-02-28 11:11:19 -0500

    new upload_audio_to_storage function which uploads audio to supabase storage

--- backend/supaDB.py
+++ backend/supaDB.py
@@ -1,4 +1,3 @@
-#entry point to supabase database
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
@@ -12,13 +11,17 @@ from crud.questions import QuestionCRUD
 from crud.user_vocabulary import UserVocabularyCRUD
 from crud.user_stories import UserStoryCRUD
 from crud.story_vocabulary import StoryVocabularyCRUD
+
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_API_KEY")
+
 if not url or not key:
     raise ValueError("Supabase URL or API Key not found.")
+
 supabase: Client = create_client(url, key)
+
 
 #tables
 user_crud = UsersCRUD(supabase)
@@ -26,20 +29,27 @@ vocabulary_crud = VocabularyCRUD(supabase)
 story_crud = StoryCRUD(supabase)
 question_crud = QuestionCRUD(supabase)
 #todo: progress_crud
-
-#bridge tables
 user_vocabulary_crud = UserVocabularyCRUD(supabase)
 user_stories_crud = UserStoryCRUD(supabase)
 story_vocabulary_crud = StoryVocabularyCRUD(supabase) #TBA if used in future
 
-#example of creating a QuestionBase object for inserting into table
-#listQuestions = models.QuestionBase(story_id=1, question_text= "what the name of goo",correct_answer= "goo", answer_choices=["cum","goo","slime","milk"])
-# storyVocab = models.StoryVocabularyBase(story_id=1, vocab_id=1)
 
-# try:
-#     story = models.StoryCreate(title="test story")
-#     result = story_crud.create_story(story)
-#     print(result)
 
-# except ValidationError as e: 
-#     print(f"KEKWWWW: {e}")
+##need to test
+def upload_audio_to_storage(story_id: int, type: str) -> str:
+
+    folder = "titles" if type == "title" else "stories"
+
+    audio_file_path = f'audio_files/{folder}/{type}_{story_id}_audio.wav'
+
+    if not os.path.exists(audio_file_path):
+        raise FileNotFoundError(f"Audio file {audio_file_path} not found.")
+
+    with open(audio_file_path, 'rb') as file:
+        storage_response = supabase.storage.from_('audio').upload(f'{folder}/{type}_{story_id}_audio.wav', file)
+
+    if storage_response.status_code != 200:
+        raise Exception(f"Error uploading audio file: {storage_response.text}")
+    
+    public_url = supabase.storage.from_('audio').get_public_url(f'{folder}/{type}_{story_id}_audio.wav')['publicURL']
+    return public_url   
