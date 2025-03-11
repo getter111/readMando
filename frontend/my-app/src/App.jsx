7f3b2d7ae32078581d3b6cf160cb08a977b7581d
Alex <ayl@g.clemson.edu>
2025-03-11 09:41:56 -0400

    added audioplayer component to play audio from supabase storage public url

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -5,21 +5,36 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SearchPage from "./pages/SearchPage"
 import StoryPage from "./pages/StoryPage"
+import PropTypes from "prop-types";
+
+function AudioPlayer({ audioUrl }) {
+  return (
+      <audio controls>
+          <source src={audioUrl} type="audio/wav" />
+          Your browser does not support the audio element.
+      </audio>
+  );
+}
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element= {<Layout />}>
-        <Route index element= {<HomePage />} />
-        <Route path="login" element= {<LoginPage />} />
-        <Route path="search/:vocab" element= {<SearchPage />} />
+    <>
+        <Routes>
+          <Route path="/" element= {<Layout />}>
+            <Route index element= {<HomePage />} />
+            <Route path="login" element= {<LoginPage />} />
+            <Route path="search/:vocab" element= {<SearchPage />} />
 
-        <Route path="story" element = {<StoryPage />} />
-      </Route>
-    </Routes>
-    
+            <Route path="story" element = {<StoryPage />} />
+          </Route>
+        </Routes>
+        <AudioPlayer audioUrl={"https://zftmkahagsvhgqkvmcop.supabase.co/storage/v1/object/public/audio/audio_files/stories/story_1_audio.wav"} />
+      </>
   )
 }
+AudioPlayer.propTypes = {
+  audioUrl: PropTypes.string.isRequired, 
+};
 
 export default App
