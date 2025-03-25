886855c1433d7506d46be3f2f52ad269dec687ed
Alex <ayl@g.clemson.edu>
2025-03-25 11:24:46 -0400

    added review route and cleaned up some things

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,20 +1,36 @@
-//import { useState } from 'react'
+// import { useState } from 'react'
 import {Routes, Route} from "react-router-dom"
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SearchPage from "./pages/SearchPage"
 import StoryPage from "./pages/StoryPage"
-import PropTypes from "prop-types";
+import ReviewPage from "./pages/ReviewPage"
 
-function AudioPlayer({ audioUrl }) {
-  return (
-      <audio controls>
-          <source src={audioUrl} type="audio/wav" />
-          Your browser does not support the audio element.
-      </audio>
-  );
-}
+import AudioPlayer from "./components/AudioPlayer"
+
+// async function addWord(word, translation, pinyin) {
+//   const [message, setMessage] = useState("");
+//   try {
+//     const response = await fetch("/vocabulary/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ word, translation, pinyin }),
+//     });
+
+//     if (response.ok) {
+//       console.log("Word added successfully");
+//       return "Word added successfully!";
+//     } else {
+//       const errorData = await response.json();
+//       return `Error: ${errorData.detail}`;
+//     }
+//   } catch (error) {
+//     console.error("Error adding vocabulary:", error);
+//     return "Error adding word.";
+//   }
+// }
+
 
 function App() {
 
@@ -25,16 +41,13 @@ function App() {
             <Route index element= {<HomePage />} />
             <Route path="login" element= {<LoginPage />} />
             <Route path="search/:vocab" element= {<SearchPage />} />
-
             <Route path="story" element = {<StoryPage />} />
+            <Route path="review" element = {<ReviewPage />} />
           </Route>
         </Routes>
         <AudioPlayer audioUrl={"https://zftmkahagsvhgqkvmcop.supabase.co/storage/v1/object/public/audio/audio_files/stories/story_1_audio.wav"} />
       </>
   )
 }
-AudioPlayer.propTypes = {
-  audioUrl: PropTypes.string.isRequired, 
-};
 
 export default App
