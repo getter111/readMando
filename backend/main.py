b2f57bc2bff58dc95694b5e7bc0afc06be92a682
Alex <ayl@g.clemson.edu>
2025-05-13 13:03:57 -0400

    implemented new function to add autofetched words to the database

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,13 @@ from uuid import uuid4
 from datetime import datetime, timedelta
 from pydantic import ValidationError
 import jieba
+import json
 
 import models
 from supaDB import user_crud, vocabulary_crud, story_crud, question_crud,user_vocabulary_crud, user_stories_crud, story_vocabulary_crud
 from storyGenerator import generateStory
 from email_utils import send_verification_email
-from utils import text_to_audio, add_vocabulary_to_db, extract_vocab 
+from utils import text_to_audio, add_vocabulary_to_db, auto_fetch
 from supaDB import upload_audio_to_storage, save_audio_url_to_db
 
 app = FastAPI()
@@ -48,6 +49,7 @@ async def generate_story(
             difficulty=request.difficulty, 
             content=story["story"]
         )
+
         created_story = story_crud.create_story(save_story)
         story_id = created_story["story_id"]
         
@@ -60,15 +62,9 @@ async def generate_story(
                     story_id= story_id,
                     read_status="FALSE"
                 )
-                user_stories_crud.create_user_story(user_story)
-        
 
-        """
-        # extract any words from the generated story and title 
-        title = extractVocab(story["title"])
-        words = extractVocab(story["story"])
-        """
-    
+                user_stories_crud.create_user_story(user_story)
+
         # use melo api to generate tts for the title and story
         title_audio_path = text_to_audio(story["title"], story_id, "title")
         story_audio_path = text_to_audio(story["story"], story_id, "story")
@@ -135,31 +131,55 @@ async def verify_email(token: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-
-    # TODO  Generate a list of comprehention questions at the end of the story
-
-# uses jieba library to segment the words in the story
-#TODO if the word is missing I want AI to:
-#predict:Part of speech,translation, and the Pinyin
-
 @app.post("/segment_story") #had to add a pydantic model/dict because in react passing json to body not a string. prev was (content:str) casuing 422 error
 async def segment_story(request: models.StorySegmentationRequest):
     try:
-        #jieba precise mode
+        #jieba precise mode to segment story into most natural words
         words = list(jieba.cut(request.content, cut_all=False))
         return {"segmented_words": words}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error segmenting story: {str(e)}")
 
-# endpoints for vocabulary onhover effect and user vocab editing
+#autofetch unknown words and add to db
+@app.post("/vocabulary/{word}")
+async def add_vocabulary(vocab: str):
+    try:
+        res = auto_fetch(vocab)
+        #Response body: "{\n  \"word\": \"诸葛亮\",\n  \"pinyin\": \"Zhūgě Liàng\",\n  \"translation\": \"Zhuge Liang\",\n  \"part of speech\": \"noun\",\n  \"example sentence\": \"诸葛亮是三国时期著名的谋略家。\"\n}"
+        if isinstance(res, str):
+            unknown_word = json.loads(res) #convert json string -> python dict
+        elif isinstance(res, dict):
+            unknown_word = res
+        else:
+            raise ValueError("Unexpected format from auto_fetch()")
+        
+        #obj representation of dict
+        new_vocab = models.VocabularyCreate(
+            word=unknown_word["word"],
+            pinyin=unknown_word["pinyin"],
+            translation=unknown_word["translation"],
+            word_type=unknown_word["part of speech"],
+            example_sentence=unknown_word["example sentence"]
+        )
+        print("res:str =")
+        print(res)
+        print("unknown_word:dict =")
+        print(unknown_word)
+
+        #upserts the unknown word
+        add_vocabulary_to_db([new_vocab.model_dump()])
+        return new_vocab
+    
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Auto-fetch failed for {vocab}: {str(e)}")
+        
+# endpoints to retrieve vocabulary for onhover effect
 @app.get("/vocabulary/{word}")
 async def get_vocabulary(word: str):
     vocab = vocabulary_crud.get_vocabulary_by_word(word)
     if vocab:
         return vocab
-    return {"vocabExists": False, "statusCode": 404}
-
-@app.post("/vocabulary")
-async def add_vocabulary(vocab: models.VocabularyCreate):
-    return vocabulary_crud.create_vocabulary(vocab)
-
+    else:
+        raise HTTPException(status_code=404, detail="Vocabulary not found.")
+    
+# TODO  Generate a list of comprehention questions at the end of the story
