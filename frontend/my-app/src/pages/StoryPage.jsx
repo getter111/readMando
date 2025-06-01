ba366083fe93452a7785feba7c6ef7ab5ab4863f
Alex Lin <ayl@g.clemson.edu>
2025-06-01 14:42:39 -0400

    added fetch story function, cached generated data, and updated the ui

--- frontend/my-app/src/pages/StoryPage.jsx
+++ frontend/my-app/src/pages/StoryPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import StoryDisplay from "../components/StoryDisplay"; 
 
@@ -8,10 +8,35 @@ export default function StoryPage() {
     const [topic, setTopic] = useState("");
     const [vocabulary, setVocabulary] = useState("");
     const [loading, setLoading] = useState(false);
+    const [loadingQuestions, setLoadingQuestions] = useState(false);
     const [error, setError] = useState("");
+
+    const [storyId, setStoryId] = useState(null);
+    const [questions, setQuestions] = useState([]);
 
     //pass in production url from netlify
     const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+    //checks if there is a cached story on the first render
+    useEffect(() => {
+        const cachedStory = localStorage.getItem("story");
+        if (cachedStory) {
+            setStory(JSON.parse(cachedStory));
+        }
+        const cachedDifficulty = localStorage.getItem("difficulty");
+        if (cachedDifficulty) {
+            setDifficulty(cachedDifficulty);
+        }
+        const cachedQuestions = localStorage.getItem("questions");
+        if (cachedQuestions) {
+            setQuestions(JSON.parse(cachedQuestions));
+        }
+    }, []);
+
+    const clearStory = () => {
+        localStorage.removeItem("story"); 
+        setStory(null);
+    }
 
     const fetchStory = async () => {
         setLoading(true);
@@ -42,8 +67,15 @@ export default function StoryPage() {
                 title: segmented_title.data.segmented_words, //title -> []
                 content: segmented_body.data.segmented_words //content -> []
             })
+            setStoryId(response.data.story_id)
 
-            // setStory(response.data);
+            //cache the story to save story in case user refreshes the page
+            localStorage.setItem("story", JSON.stringify({
+                title: segmented_title.data.segmented_words,
+                content: segmented_body.data.segmented_words
+            }))
+            localStorage.setItem("difficulty", difficulty);
+
         } catch (error) {
             setError("Failed to generate story. Try again!");
             console.error(error);
@@ -51,6 +83,29 @@ export default function StoryPage() {
             setLoading(false);//reset loading state
         }
     };
+
+    const fetchQuestions = async () => {
+        setLoadingQuestions(true);
+        setError("");
+        try {
+            const response = await axios.post(`${apiUrl}/generate_questions`, {
+                story: story.content.join(""), // convert content & title array into a string
+                title: story.title.join(""), 
+                difficulty: difficulty,
+                story_id: storyId
+            });
+
+            console.log("Questions generated: ", response.data);
+            setQuestions(response.data)
+            localStorage.setItem("questions", JSON.stringify(response.data));
+        } catch (error) {
+            setError("Failed to generate questions. Try again!");
+            console.error(error);
+        } finally {
+            setLoadingQuestions(false);
+        }
+
+    }
 
     return (
         <div className="bg-gray-100 min-h-screen flex flex-col">
@@ -107,18 +162,22 @@ export default function StoryPage() {
                         {error && <p className="text-red-500 mt-2">{error}</p>}
                     </div>
 
-                    <div className="bg-white p-6 rounded-lg shadow-md">
+                    <div className="bg-white p-6 rounded-lg shadow-md flex flex-col justify-between">
                         <h2 className="text-xl font-semibold mb-4">Generated Story:</h2>
 
                         {loading ? (
-                            <p className="text-gray-500">Generating story...</p>
+                            <div className="text-gray-500 flex-auto">Generating story...</div>
                         ) : story ? (
-                            <StoryDisplay story={story} />
+                            <StoryDisplay story={story} fetchQuestions={fetchQuestions} loadingQuestions={loadingQuestions} clearStory={clearStory}/>
                         ) : (
-                            <p className="text-gray-500">No story generated yet.</p>
+                            <div className="text-gray-500 flex-auto">No story generated yet.</div>
                         )}
                     </div>
                 </div>
+                <div className="bg-white mt-6 p-6 rounded-lg shadow-md">
+                    
+                </div>
+
             </main>
         </div>
     );
