d44ad228d4d25df47d339afa37e79364bc999300
Alex Lin <ayl@g.clemson.edu>
2025-06-06 17:44:03 -0400

    added new routes and a user state

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -6,46 +6,32 @@ import LoginPage from './pages/LoginPage'
 import SearchPage from "./pages/SearchPage"
 import StoryPage from "./pages/StoryPage"
 import ReviewPage from "./pages/ReviewPage"
+import VerificationSuccessPage from "./pages/VerificationSuccessPage"
+import RegisterPage from "./pages/RegisterPage"
 
-import AudioPlayer from "./components/AudioPlayer"
-
-// async function addWord(word, translation, pinyin) {
-//   const [message, setMessage] = useState("");
-//   try {
-//     const response = await fetch("/vocabulary/", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ word, translation, pinyin }),
-//     });
-
-//     if (response.ok) {
-//       console.log("Word added successfully");
-//       return "Word added successfully!";
-//     } else {
-//       const errorData = await response.json();
-//       return `Error: ${errorData.detail}`;
-//     }
-//   } catch (error) {
-//     console.error("Error adding vocabulary:", error);
-//     return "Error adding word.";
-//   }
-// }
-
+import { useState, useEffect } from "react"
 
 function App() {
+  const [user, setUser] = useState({"user": "guest"});
+
+  useEffect(() => {
+    console.log("Current user:", user);
+  },  [user]);
 
   return (
     <>
         <Routes>
           <Route path="/" element= {<Layout />}>
             <Route index element= {<HomePage />} />
-            <Route path="login" element= {<LoginPage />} />
+            <Route path="login" element= {<LoginPage setUser={setUser} />} />
+            <Route path="register" element= {<RegisterPage />} />
             <Route path="search/:vocab" element= {<SearchPage />} />
-            <Route path="story" element = {<StoryPage />} />
+            <Route path="story" element = {<StoryPage user_id={user.user_id}/>} />
             <Route path="review" element = {<ReviewPage />} />
+            <Route path="verification-success" element={<VerificationSuccessPage />} />
           </Route>
         </Routes>
-        <AudioPlayer audioUrl={"https://zftmkahagsvhgqkvmcop.supabase.co/storage/v1/object/public/audio/audio_files/stories/story_4_audio.wav?"} />
+        {/* <AudioPlayer audioUrl={"https://zftmkahagsvhgqkvmcop.supabase.co/storage/v1/object/public/audio/audio_files/stories/story_4_audio.wav?"} /> */}
       </>
   )
 }
