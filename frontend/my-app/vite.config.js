a8f5959bc82e1d70d50284916d28753ae2d7ea93
Alex Lin <ayl@g.clemson.edu>
2025-06-10 22:46:26 -0400

    updated config for local https testing

--- frontend/my-app/vite.config.js
+++ frontend/my-app/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      "/api": "http://localhost:8000", //redirect any traffic starting with /api to url on the right during dev
+    https: {
+      key: fs.readFileSync('./localhost+1-key.pem'),
+      cert: fs.readFileSync('./localhost+1.pem'),
     },
+    port: 5173,
   },
 })
