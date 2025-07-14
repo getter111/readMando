914189a00897bc25c0151fa819c4ecdcf4ee6f02
Alex Lin <ayl@g.clemson.edu>
2025-07-14 14:08:33 -0400

    fix deploy issue

--- frontend/my-app/src/pages/StudyPage.jsx
+++ frontend/my-app/src/pages/StudyPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import SearchBar from "../components/Searchbar";
+import SearchBar from "../components/SearchBar";
 import PropTypes from "prop-types";
 import axios from "axios";
 
